093db23e7bae81e2d3dfaeaf4ca1c0cde14bc540
hyori1004 <jenny7983@naver.com>
2021-09-28 01:07:23 +0900

    3004

--- app/src/main/java/com/example/firstproject3/Login/UserAccount.java
+++ app/src/main/java/com/example/firstproject3/Login/UserAccount.java
@@ -8,14 +8,20 @@ package com.example.firstproject3.Login;
 public class UserAccount
 {
     private String idToken;     // Firebase Uid (고유 토큰정보)
+    private String username;        // 이름
     private String emailId;     // 이메일 아이디
     private String password;    // 비밀번호
+    private String repassword; //비밀번호 확인
 
     public UserAccount() { }
 
     public String getIdToken() { return idToken; }
 
     public void setIdToken(String idToken) { this.idToken = idToken; }
+
+    public String getUserName() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
 
     public String getEmailId() { return emailId; }
 
@@ -25,5 +31,8 @@ public class UserAccount
 
     public void setPassword(String password) { this.password = password; }
 
+    public String getRepassword() { return repassword; }
+
+    public void setRepassword(String repassword) {this.repassword = repassword; }
 
 }
