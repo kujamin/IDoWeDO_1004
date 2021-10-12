0b313c62dfe01bb74fcf32e60c30fd443d39f604
Second <hise13>
2021-10-12 17:13:54 +0900

    upload

--- app/src/main/java/com/example/firstproject3/AtCheck/Attendance_CheckActivity.java
+++ app/src/main/java/com/example/firstproject3/AtCheck/Attendance_CheckActivity.java
@@ -8,13 +8,32 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.example.firstproject3.Login.UserAccount;
 import com.example.firstproject3.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
+import java.sql.Date;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Attendance_CheckActivity extends Activity {
     private TextView monthYearText;
@@ -22,12 +41,22 @@ public class Attendance_CheckActivity extends Activity {
     private CalendarDay date;
     Cursor cursor;
     MaterialCalendarView calendarView;
+    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증처리
+    private DatabaseReference mDatabase;
+    private String usercode, dateR, datee;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.activity_attendance_check);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         calendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
 
@@ -43,7 +72,37 @@ public class Attendance_CheckActivity extends Activity {
         calendarView.addDecorators(
                 new SundayDecorator(),
                 new SaturdayDecorator()
-                );
+        );
+
+        //usercode 얻어오기
+        mDatabase.child("idowedo").child("UserAccount").child(firebaseUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserAccount group = dataSnapshot.getValue(UserAccount.class);
+                usercode = (group.getEmailid());
+
+                //달력에 동그라미 남기기
+                firebaseFirestore.collection("user").document(usercode).collection("user Check")
+                        .whereEqualTo("checkOX", "O")
+                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                datee = document.getString("checkDate");
+                                Date d = Date.valueOf(datee);
+                                calendarView.setSelectedDate(d);
+                            }
+                        }
+                    }
+                });
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
         //출석체크 버튼
         Button attenBtn = (Button) findViewById(R.id.checkbutton);
@@ -51,8 +110,37 @@ public class Attendance_CheckActivity extends Activity {
             @Override
             public void onClick(View v) {
                 calendarView.setSelectedDate(CalendarDay.today());
+                String year = String.valueOf(CalendarDay.today().getYear());
+                String month = String.valueOf(CalendarDay.today().getMonth() + 1);
+                String day = String.valueOf(CalendarDay.today().getDay());
+
+                if (month.length() != 2) {
+                    month = 0 + month;
+                }
+                if (day.length() != 2){
+                    day = 0 + day;
+                }
+
+                dateR = year + "-" + month + "-" + day;
+                Toast.makeText(getApplicationContext(), dateR, Toast.LENGTH_SHORT).show();
+
+                Map<String, Object> doc = new HashMap<>();
+                doc.put("checkOX", "O");
+                doc.put("usercode", usercode);
+                doc.put("checkDate", dateR);
+
+                firebaseFirestore.collection("user").document(usercode).collection("user Check").document(dateR).set(doc)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                            }
+                        });
+
             }
         });
+
+
     }
 
 
